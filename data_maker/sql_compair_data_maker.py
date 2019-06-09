2b62a92765b04d0b00c1fd7a93b3cc05a0476938
LINDBURG <monc2170@gmail.com>
2019-06-09 18:20:21 +0900

    revert sql_compair

--- data_maker/sql_compair_data_maker.py
+++ data_maker/sql_compair_data_maker.py
@@ -57,6 +57,38 @@ for fname in fnames1:
             check +=1
             flag =1
     jsonfile.close()
+    
+
+for fname in fnames2:
+    jsonfile = open(dir_name+ '/' + fname, 'r', encoding="utf-8")
+    json_data = json.load(jsonfile)
+
+    file_data = OrderedDict()
+    
+    print("processing_1: " + fname)
+    check =0
+    for data in json_data:
+        msg = data['MSG']
+        if not msg_check.msg_check(msg, sql_inject_key,sql_inject_nkey):
+            if flag == 1 and check ==0:
+                sql_json_1.write(",\n")
+            if check !=0:
+                sql_json_1.write(",\n")
+            file_data["FILE"] = fname
+            file_data["MSG"] = data["MSG"]
+            file_data["RES"] = data["RES"]
+            tmp = []
+            for payload in data["PAYLOAD"]:
+                tempdata = []
+                for innerdata in payload["DATA"]:
+                    tempdata.append(innerdata)
+                tmp.append(tempdata)
+            file_data["DATA"] = tmp
+            json.dump(file_data, sql_json_1, indent=4)
+            check +=1
+            flag =1
+    jsonfile.close()
+    
 
 sql_json_1.write("\n]")
 sql_json_1.close()
